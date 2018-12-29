8a8b9cc27a476d7593c0f7da40519835cd17e92b
wzjing <wzjing103@gmail.com>
2018-12-29 09:08:47 +0800

    use looper control the decode thread

--- app/src/main/java/com/wzjing/scan/MainActivity.java
+++ app/src/main/java/com/wzjing/scan/MainActivity.java
@@ -1,209 +1,267 @@
 package com.wzjing.scan;
 
-import android.annotation.SuppressLint;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.AsyncTask;
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.google.zxing.BarcodeFormat;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.FormatException;
-import com.google.zxing.NotFoundException;
+import com.google.zxing.MultiFormatReader;
 import com.google.zxing.PlanarYUVLuminanceSource;
-import com.google.zxing.Reader;
+import com.google.zxing.ReaderException;
 import com.google.zxing.Result;
+import com.google.zxing.client.android.camera.CameraConfigurationUtils;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
-    private final String TAG = "ScanTest";
+    private SurfaceView surfaceView;
 
-    private Button scanBtn;
-    private TextView resultTv;
-    private ImageView iv;
+    private Camera camera;
+    private Rect scanRect;
+    private Rect clipRect;
+    private byte[] buffer;
+    private MultiFormatReader reader;
+    private int screenWidth;
+    private int screenHeight;
+
+    private ScanThread thread;
+    private CountDownLatch countDown;
+
+    private final int PERMISSION_CODE = 0x101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        scanBtn = (Button) findViewById(R.id.scan_btn);
-        resultTv = (TextView) findViewById(R.id.result_tv);
-        iv = (ImageView) findViewById(R.id.imageView);
+        ld("Activity Create");
+        setContentView(R.layout.activity_scan);
+        ImageView scanIv = findViewById(R.id.scanIv);
+        surfaceView = findViewById(R.id.surfaceView);
+
+        thread = new ScanThread();
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+        countDown = new CountDownLatch(1);
+        scanIv.post(() -> {
+            scanRect = new Rect(scanIv.getLeft(), scanIv.getTop(), scanIv.getRight(), scanIv.getBottom());
+            countDown.countDown();
+        });
+        surfaceView.getHolder().addCallback(this);
     }
 
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.scan_btn:
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, 100);
-                resultTv.setText("选择图片中");
-                break;
-        }
-    }
-
-    @SuppressLint("StaticFieldLeak")
     @Override
-    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100 && resultCode == RESULT_OK) {
-            scanBtn.setClickable(false);
-            final Uri uri = data.getData();
-            final ContentResolver cr = getContentResolver();
-            new AsyncTask<Uri, Bitmap, String>() {
-                @Override
-                protected void onPreExecute() {
-                    super.onPreExecute();
-                    resultTv.setText("开始二值化...");
-                    iv.setImageBitmap(null);
-                }
+    protected void onStart() {
+        super.onStart();
+        li("Activity Start");
+    }
 
-                @Override
-                protected String doInBackground(Uri... params) {
-                    try {
-                        Bitmap bitmap = gray2Binary(reduceBitmap(cr.openInputStream(params[0])));
-                        publishProgress(bitmap);
-                        int[] pixels = new int[bitmap.getWidth() * bitmap.getHeight()];
-                        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-                        Log.i(TAG, "Step1");
-                        int[] pix = new int[bitmap.getWidth() * bitmap.getHeight()];
-                        bitmap.getPixels(pix, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-                        byte[] yuv = new byte[pix.length];
-                        QrUtil.argb2YUV420sp(yuv, pix, bitmap.getWidth(), bitmap.getHeight());
-                        PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(yuv, bitmap.getWidth(), bitmap.getHeight(), 0, 0, bitmap.getWidth(), bitmap.getHeight(), false);
-//                        LuminanceSource source = new RGBLuminanceSource(bitmap.getWidth(), bitmap.getHeight(), pixels);
-                        Log.i(TAG, "Step2");
-                        BinaryBitmap bb = new BinaryBitmap(new HybridBinarizer(source));
-                        Log.i(TAG, "Step3");
-                        Log.i(TAG, "Step4");
-                        Map<DecodeHintType, Object> hint = new HashMap<>();
-                        hint.put(DecodeHintType.CHARACTER_SET, "utf-8");
-                        hint.put(DecodeHintType.TRY_HARDER, true);
-//                        hint.put(DecodeHintType.PURE_BARCODE, false);
-                        List<BarcodeFormat> formats = new ArrayList<>();
-                        formats.add(BarcodeFormat.QR_CODE);
-                        hint.put(DecodeHintType.POSSIBLE_FORMATS, formats);
-                        Reader reader = new QRCodeReader();
-                        Result result = reader.decode(bb, hint);
-                        return result.getText();
-                    } catch (FileNotFoundException e) {
-                        Log.e(TAG, "Exception while resolve picture data!");
-                        return "Exception while resolve picture data!";
-                    } catch (FormatException e) {
-                        Log.e(TAG, "Exception-FormatException");
-                        return "Exception-FormatException";
-                    } catch (ChecksumException e) {
-                        Log.e(TAG, "Exception-ChecksumException");
-                        return "Exception-ChecksumException";
-                    } catch (NotFoundException e) {
-                        Log.e(TAG, "Exception-NotFoundException");
-                        return "Exception-NotFoundException";
-                    } catch (IOException e) {
-                        Log.e(TAG, "Exception-IOException");
-                        return "Exception-IOException";
-                    }
-                }
+    @Override
+    protected void onResume() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_CODE);
+        }
+        super.onResume();
+        li("Activity Resume");
+    }
 
-                @Override
-                protected void onProgressUpdate(Bitmap... values) {
-                    super.onProgressUpdate(values);
-                    ((ImageView) (findViewById(R.id.imageView))).setImageBitmap(values[0]);
-                    resultTv.setText("二值化完成....");
-                }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        li("Activity Pause");
+    }
 
-                @Override
-                protected void onPostExecute(String aString) {
-                    super.onPostExecute(aString);
-                    resultTv.setText(aString);
-                    scanBtn.setClickable(true);
-                }
-            }.execute(uri);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        li("Activity Destroy");
+    }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        li("Surface Created");
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        li("Surface Change");
+        startCamera();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        li("Surface Destroy");
+        stopCamera();
+    }
+
+    private void startCamera() {
+        camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+        Camera.Parameters params = camera.getParameters();
+        CameraConfigurationUtils.findBestPreviewSizeValue(params,
+                new Point(getResources().getDisplayMetrics().widthPixels,
+                        getResources().getDisplayMetrics().heightPixels));
+        CameraConfigurationUtils.setFocus(params, true, false, false);
+        CameraConfigurationUtils.setBarcodeSceneMode(params);
+        CameraConfigurationUtils.setFocusArea(params);
+
+        camera.setParameters(params);
+        camera.setDisplayOrientation(90);
+        int w = camera.getParameters().getPreviewSize().width;
+        int h = camera.getParameters().getPreviewSize().height;
+        int size = (int) (w * h * 1.5);
+        buffer = new byte[size];
+        camera.addCallbackBuffer(buffer);
+        camera.setPreviewCallbackWithBuffer((buffer, camera) -> {
+//            ld("onCameraPreview()");
+            Message.obtain(thread.getHandler(), 0, h, w, buffer).sendToTarget();
+        });
+        reader = new MultiFormatReader();
+        try {
+            camera.setPreviewDisplay(surfaceView.getHolder());
+            camera.startPreview();
+            thread.start();
+        } catch (IOException e) {
+            le("Error: %s", e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public Bitmap gray2Binary(Bitmap graymap) {
-        //得到图形的宽度和长度
-        int width = graymap.getWidth();
-        int height = graymap.getHeight();
-        //创建二值化图像
-        Bitmap binarymap = null;
-        binarymap = graymap.copy(Bitmap.Config.ARGB_8888, true);
-        //依次循环，对图像的像素进行处理
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                //得到当前像素的值
-                int col = binarymap.getPixel(i, j);
-                //得到alpha通道的值
-                int alpha = col & 0xFF000000;
-                //得到图像的像素RGB的值
-                int red = (col & 0x00FF0000) >> 16;
-                int green = (col & 0x0000FF00) >> 8;
-                int blue = (col & 0x000000FF);
-                // 用公式X = 0.3×R+0.59×G+0.11×B计算出X代替原来的RGB
-                int gray = (int) ((float) red * 0.3 + (float) green * 0.59 + (float) blue * 0.11);
-                //对图像进行二值化处理
-                if (gray <= 25) {
-                    gray = 0;
+    private void stopCamera() {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+        }
+        if (buffer != null) {
+            buffer = null;
+        }
+        if (thread != null) {
+            thread.cancel();
+        }
+    }
+
+    private class ScanThread extends Thread {
+        private Looper mLooper;
+        private Handler handler;
+
+        ScanThread() {
+            super();
+            setName("MyLooper");
+        }
+
+        Handler getHandler() {
+            return handler;
+        }
+
+        @Override
+        public void run() {
+            ld("run()");
+            Looper.prepare();
+            ld("Looper prepared");
+            mLooper = Looper.myLooper();
+            handler = new ScanHandler(Looper.myLooper());
+            Looper.loop();
+            ld("Looper Finished");
+        }
+
+        void cancel() {
+            Objects.requireNonNull(mLooper).quit();
+        }
+    }
+
+    private class ScanHandler extends Handler {
+        ScanHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            int w = msg.arg1;
+            int h = msg.arg2;
+            byte[] data = (byte[]) msg.obj;
+            if (clipRect == null) {
+                clipRect = new Rect();
+                clipRect.left = w * scanRect.left / screenWidth;
+                clipRect.top = h * scanRect.top / screenHeight;
+                clipRect.right = w * scanRect.right / screenWidth;
+                clipRect.bottom = h * scanRect.bottom / screenHeight;
+            }
+            ld("handleMessage: %dx%d %s %d", w, h, scanRect.toShortString(), data.length);
+            PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(rotateYUV420Degree90(data, h, w), w, h,
+                    clipRect.left, clipRect.top, clipRect.width(), clipRect.height(), false);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                Result result = reader.decode(bitmap);
+                if (result != null) {
+                    ld("Decoded Message: " + result.getText());
                 } else {
-                    gray = 255;
+                    ld("Decoded Message: NULL");
                 }
-                // 新的ARGB
-                int newColor = alpha | (gray << 16) | (gray << 8) | gray;
-                //设置新图像的当前像素值
-                binarymap.setPixel(i, j, newColor);
+            } catch (ReaderException e) {
+                ld("handleMessage: Exception-" + e.getClass().getSimpleName());
+                // Keep trying
+                runOnUiThread(() -> camera.addCallbackBuffer(buffer));
+            } finally {
+                reader.reset();
             }
         }
-        return binarymap;
     }
 
-    public Bitmap reduceBitmap(InputStream is) throws IOException {
-        int length = is.available();
-        byte[] buffer = new byte[length];
-        is.read(buffer, 0, length);
-        BitmapFactory.Options option = new BitmapFactory.Options();
-        option.inJustDecodeBounds = true;
-        BitmapFactory.decodeByteArray(buffer, 0, length, option);
-
-        option.inJustDecodeBounds = false;
-        int width = option.outWidth;
-        int height = option.outHeight;
-        int maxWidth = 800;
-        float bitmapRatio = (float) width / maxWidth;
-
-        int simpleSize = (int) Math.floor(bitmapRatio);
-        if ((simpleSize % 2) == 1) {
-            simpleSize += 1;
+    private byte[] rotateYUV420Degree90(byte[] data, int imageWidth, int imageHeight)
+    {
+        byte [] yuv = new byte[imageWidth*imageHeight*3/2];
+        // Rotate the Y luma
+        int i = 0;
+        for(int x = 0;x < imageWidth;x++)
+        {
+            for(int y = imageHeight-1;y >= 0;y--)
+            {
+                yuv[i] = data[y*imageWidth+x];
+                i++;
+            }
         }
-
-        option.inSampleSize = simpleSize;
-        if (width > maxWidth) {
-            option.outWidth = maxWidth;
-            option.outHeight = maxWidth * height / width;
-//            option.inPreferredConfig = Bitmap.Config.RGB_565;
+        // Rotate the U and V color components
+        i = imageWidth*imageHeight*3/2-1;
+        for(int x = imageWidth-1;x > 0;x=x-2)
+        {
+            for(int y = 0;y < imageHeight/2;y++)
+            {
+                yuv[i] = data[(imageWidth*imageHeight)+(y*imageWidth)+x];
+                i--;
+                yuv[i] = data[(imageWidth*imageHeight)+(y*imageWidth)+(x-1)];
+                i--;
+            }
         }
+        return yuv;
+    }
 
-        return BitmapFactory.decodeByteArray(buffer, 0, length, option);
+
+    private final String TAG = "ScanActivity";
+
+    private void ld(String format, Object... args) {
+        Log.d(TAG, String.format(format, args));
+    }
+
+    private void li(String format, Object... args) {
+        Log.i(TAG, String.format(format, args));
+    }
+
+    private void le(String format, Object... args) {
+        Log.e(TAG, String.format(format, args));
     }
 }
